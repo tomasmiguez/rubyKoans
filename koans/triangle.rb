e40392de9b41a707f6def4735d0565530a4826a3
Tomas Miguez <tomasmiguez99@gmail.com>
2021-12-09 17:56:08 -0300

    about_triangle_project_2 resuelto

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,6 +14,14 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  sides = [a, b, c]
+  
+  if sides.any? { |side| side <= 0}
+    raise TriangleError.new("Lengths can´t be less than 0.\n")
+  elsif sides.max() >= sides.sum - sides.max()
+    raise TriangleError.new("The side of length #{sides.max()} is longer than the sum of #{sides - [sides.max()]}.\n")
+  end
+
   if a == b and b == c
     :equilateral
   elsif a == b or b == c or c == a
